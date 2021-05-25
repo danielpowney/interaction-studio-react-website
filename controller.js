50820ad15b9783bb311455bb89c4166ea777f0bd
Daniel Powney <danielpowney@gmail.com>
2021-05-26 09:59:39 +1000

    Formatting updates

--- controller.js
+++ controller.js
@@ -11,52 +11,52 @@ var BannerWithCTA2 = require('./public/js/banner-with-cta2');
  */
 var controller = {
 
-	/**
-	 * Serves initial page display
-	 */
-	index : async function(req, res) {
-		
-		// Get server side campaign
-		let campaigns = [];
+    /**
+     * Serves initial page display
+     */
+    index : async function(req, res) {
 
-		const request = require('request-promise');
-	    const options = {
-	        url : process.env.IS_AUTHEVENT_URL,
-	        method : 'POST',
-	        headers: {
-		        'Content-Type': 'application/json',
-		        'Authorization': 'Basic ' + process.env.IS_API_CREDENTIALS
-		    },
-	        body : JSON.stringify({ 
-			    "action": "hello world", 
-			    "source": { 
-			        "channel": "Server" 
-			    },
-			    "user": {
-			        "id": "testuser"
-			    }
-			}) 
-	    };
+        // Get server side campaign
+        let campaigns = [];
 
-	    try {
-	    	var response = await request(options);
-	        const jsonResponse = JSON.parse(response);
-	        campaigns = jsonResponse.campaignResponses;
-	    } catch (e) {
-	    	console.log(e);
-	    }
+        const request = require('request-promise');
+        const options = {
+            url : process.env.IS_AUTHEVENT_URL,
+            method : 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + process.env.IS_API_CREDENTIALS
+            },
+            body : JSON.stringify({ 
+                    "action": "hello world", 
+                    "source": { 
+                    "channel": "Server" 
+                },
+                "user": {
+                    "id": "testuser"
+                }
+            }) 
+        };
 
-	   	let props = {
-	    	ctaUrl: campaigns[0].payload.ctaUrl,
+        try {
+            var response = await request(options);
+            const jsonResponse = JSON.parse(response);
+            campaigns = jsonResponse.campaignResponses;
+        } catch (e) {
+            console.log(e);
+        }
+
+        let props = {
+            ctaUrl: campaigns[0].payload.ctaUrl,
             subheader: campaigns[0].payload.subheader,
             ctaText: campaigns[0].payload.ctaText,
             imageURL: campaigns[0].payload.imageURL,
             header: campaigns[0].payload.header
-	    };
-		const html = ReactDOMServer.renderToString(React.createElement(BannerWithCTA2, props));
+        };
+        const html = ReactDOMServer.renderToString(React.createElement(BannerWithCTA2, props));
 
-		res.render("pages/index", { campaigns : [ { html : html } ], beaconURL : process.env.IS_BEACON_URL } );
-	}
+        res.render("pages/index", { campaigns : [ { html : html } ], beaconURL : process.env.IS_BEACON_URL } );
+    }
 
 }
 
